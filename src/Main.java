d34c3a75f5a4d6beb1676c914518a559806406de
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-09-20 01:27:57 +0530

    if else if implement

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import com.thedevsaroj.Hello;
 import com.thedevsaroj.JavaIf;
 
 public class Main {
@@ -9,5 +10,18 @@ public class Main {
         int num2 = 30;
         int result = num1 + num2;
         System.out.println(result);
+        Hello hl = new Hello();
+        System.out.println(hl);
+
+        int n= 6;
+        if (n==0) {
+            System.out.println("nothing");
+        } else if (n%2==0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
+
     }
 }
--- src/com/thedevsaroj/Hello.java
+++ src/com/thedevsaroj/Hello.java
@@ -1,5 +1,12 @@
 package com.thedevsaroj;
 
 public class Hello {
+    public Hello(){
+        System.out.println("Hello object created");
+    }
 
+    @Override
+    public String toString() {
+        return "Hello{}";
+    }
 }
